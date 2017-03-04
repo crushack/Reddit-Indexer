c067822ed3d667784a228a76a9973985631b95e2
crushack <psilviu94@gmail.com>
2017-03-05 01:27:45 +0200

    Intexes tests added

--- mongodb_interface_test.py
+++ mongodb_interface_test.py
@@ -1,7 +1,7 @@
 import unittest
 
 import mongodb_interface
-from mock import Mock, MagicMock
+from mock import call, Mock, MagicMock
 
 class TestQueries(unittest.TestCase):
     FROM_TIMESTAMP = 1
@@ -70,7 +70,41 @@ class TestQueries(unittest.TestCase):
             TestQueries.WITHOUT_KEY_QUERRY)
 
 
-class TestInsertions(unittest.TestCase):
+class TestInsertionIndexes(unittest.TestCase):
+    SUBREDDIT = 'all'
+
+    def setUp(self):
+        ensure_index = Mock()
+
+        collection = MagicMock()
+        collection.ensure_index = ensure_index
+
+        database = MagicMock()
+        database.__getitem__.return_value = collection
+
+        client = MagicMock()
+        client.__getitem__.return_value = database
+
+        calls = []
+        for search_index in mongodb_interface.SEARCH_INDEXES:
+            calls.append(call(search_index, default_language='none'))
+
+        self.calls = calls
+        self.client = client
+        self.ensure_index = ensure_index
+
+    def test_insert_submissions_indexes(self):
+        mongodb_interface.insert_submissions(
+            self.client, TestInsertionIndexes.SUBREDDIT, [])
+        self.ensure_index.assert_has_calls(self.calls)
+
+    def test_insert_comments_indexes(self):
+        mongodb_interface.insert_comments(
+            self.client, TestInsertionIndexes.SUBREDDIT, [])
+        self.ensure_index.assert_has_calls(self.calls)
+
+
+class TestInit(unittest.TestCase):
     pass
 
 
